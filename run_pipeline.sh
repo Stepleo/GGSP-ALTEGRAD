376c0f814be4e4b10678ea179ab0481b85d4d14b
StepLeo <stepleo66@gmail.com>
2025-04-20 17:19:45 +0200

    Add timeout and grid search

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -9,4 +9,4 @@ python /app/mlflow/experiments/evaluate.py
 
 # Step 2: Launch the FastAPI application
 echo "Launching the FastAPI application..."
-exec uvicorn api.main:app --host 0.0.0.0 --port 8000
+exec uvicorn api.main:app --host 0.0.0.0 --port 8000 --timeout 600
